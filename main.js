b0f7eb8a66f26e911a60057c1799e1f800938693
ron erlih <ronerlih@gmail.com>
2021-02-18 12:40:02 +0200

    initial commit- window package and style

---
+++ main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow, screen, nativeImage } = require("electron");
+const SCREEN_WIDTH = 600;
+
+function createWindow() {
+	const win = new BrowserWindow({
+		width: SCREEN_WIDTH,
+		height: 300,
+      title: "slack overlay",
+		transparent: true,
+		// titleBarStyle: "dark",
+		alwaysOnTop: true,
+      frame: false,
+      name: "slack overlay",
+      shadow: false,
+      hasShadow: false,
+		zoomToPageWidth: true,
+      // border: 10,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
+
+	const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+	win.setPosition(width - SCREEN_WIDTH, 0);
+   win.loadFile("index.html");
+
+   // DEBUGGING - dev tools
+   // win.webContents.openDevTools()
+}
+
+// set icon for mac
+const image = nativeImage
+   .createFromPath(app.getAppPath() + "/build/icon.png");
+app.dock.setIcon(image);
+   
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+app.on("activate", () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
+});
